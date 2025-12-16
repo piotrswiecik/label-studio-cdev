a97e4fef93b50fba66b8d56da7161c81bb1eb5bb
Piotr Święcik <piotr.swiecik@gmail.com>
2025-12-16 09:33:41 +0100

    enforce tag length limit

--- label_studio/projects/models.py
+++ label_studio/projects/models.py
@@ -160,7 +160,7 @@ recalculate_all_stats = load_func(settings.RECALCULATE_ALL_STATS)
 
 
 class ProjectTag(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
